ac8fdacc9b0023225fd85b49d49c3b4e24387ed9
Mark Raiter <raitermrk@gmail.com>
2023-11-22 23:18:37 +0200

    added config and logger

---
+++ cmd/auth_service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/markraiter/auth-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("config", cfg))
+
+	// TODO: initialize app
+
+	// TODO: start gRPC-sever application
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
